6ee9b914b611769c34ae3c6535c10aa484e37481
sheharyarroshan <aliroshan000@gmail.com>
2023-11-22 01:50:09 +0500

    in process

---
+++ lib/screen/privacyPolicy/termsAndCondition.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+
+import 'package:vpn_app/constants/stringConst.dart';
+
+class LicenseAggrement extends StatelessWidget {
+  static const routeName = '/License';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('License  Agrement'),
+      ),
+      body: SingleChildScrollView(
+        child: Html(
+          data: StringConst.LISCENSAGREEMENT,
+        ),
+      ),
+    );
+  }
+}
